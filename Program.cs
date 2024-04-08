e664f8ab7d637c4013e60e8651ca23cb1c1e8bb3
Rails47 <158601407+Rails47@users.noreply.github.com>
2024-04-08 14:46:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient server = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 12345);
+
+            Thread sendThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    byte[] data = Encoding.ASCII.GetBytes(DateTime.Now.ToString());
+                    server.Send(data, data.Length, endPoint);
+                    Thread.Sleep(1000); 
+                }
+            });
+            sendThread.Start();
+
+            while (true)
+            {
+                byte[] receivedData = server.Receive(ref endPoint);
+                string receivedText = Encoding.ASCII.GetString(receivedData);
+                Console.WriteLine("Received: " + receivedText);
+            }
+        }
+    }
+}
